674e6a2bbca653821293a27b2c36232e4c4f45e3
A.J <56674434+Aveeshajay@users.noreply.github.com>
2021-05-29 03:10:45 -0700

    Add files via upload

---
+++ src/stockmanagement/Item.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockmanagement;
+
+/**
+ *
+ * @author Yasas
+ */
+public class Item {
+   private String item_code;
+   private String type;
+   private String name;
+   private int quantity;
+   private String supplier;
+   private int reorder_level;
+   private float price;
+    
+    Item(String item_code,String type,String name,int quantity,String supplier,int reorder_level,float price)
+    {
+        this.item_code = item_code;
+        this.type = type;
+        this.name = name;
+        this.quantity = quantity;
+        this.supplier = supplier;
+        this.reorder_level = reorder_level;
+        this.price = price;
+        
+        
+    }
+    
+    String getCode()
+    {
+        return this.item_code;
+    }
+    
+    String getType()
+    {
+        return this.type;
+    }
+    
+    String getName()
+    {
+        return this.name;
+    }
+    
+    int getquantity()
+    {
+        return this.quantity;
+    }
+    
+    String getSupplier()
+    {
+        return this.supplier;
+    }
+    
+    int getReLevel()
+    {
+        return this.reorder_level;
+    }
+    
+    float getUnPrice()
+    {
+        return this.price;
+    }
+    
+    
+    
+}
